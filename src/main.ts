42e76273b0d85009ea5b2ac8ba580adb89e2357b
Michael Smith Jr <mj3397@gmail.com>
2018-12-17 16:42:05 -0600

    remove service worker for the time being

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
 import axios from './server-axios'
 
 Vue.prototype.$http = axios
